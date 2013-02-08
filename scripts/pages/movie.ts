98aff464169027d044881b284500128487868a73
jessicawong <jessica.wong@macys.com>
2013-02-08 11:22:41 -0800

    changes!

--- scripts/pages/movie.ts
+++ scripts/pages/movie.ts
@@ -68,4 +68,53 @@ $("/html/body") {
 	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[3]/div/div[3]/a"){
 		add_class("orangeButton")
 	}
+
+	$("//i[contains(@class, 'popcorn')]") {
+		attribute("class", "icon huge popcorn img mrs")
+	}
+
+	$("//i[contains(@class, 'rotten')]") {
+		attribute("class", "icon huge rotten img mrs")
+	}
+
+	$("//div[contains(@class,'box')]/div[contains(@class,'media')]//p[2]") {
+		remove()
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[2]/div/div/div/ul/li[1]") {
+		attribute("style", "width:50%; color:#FF5C00")
+	}
+	
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[2]/div/div/div/ul/li[2]") {
+		attribute("style", "width:50%; color:#FF5C00")
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[2]/div/div/div/ul/li[1]/p/span[2]") {
+		attribute("class", "medium")
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[2]/div/div/div/ul/li[2]/p/span[2]") {
+		attribute("class", "medium")
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[3]/div/h3") {
+		remove()
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[3]/div/a") {
+		remove()
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[3]/div/div[1]") {
+		attribute("class", "medium")
+		add_class("stdef")
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[2]/div/div/div/div") {
+		add_class("desCredits")
+	}
+
+	$("//*[@id='mwMovie']/div[2]/div[2]/div[1]/div[3]/div/p/a") {
+		add_class("desCredits")
+	}
 }
